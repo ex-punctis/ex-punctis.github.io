72b55525e5b1eae65ce171385bf0bdc03c08e2e5
ex-punctis <37907966+ex-punctis@users.noreply.github.com>
2018-07-25 15:36:20 -0400

    readme and js formatting

--- _includes/img-zoom.js
+++ _includes/img-zoom.js
@@ -12,42 +12,42 @@ function draw_expanded_img() {
 
 	aspectRatio = imgExpanded.width/imgExpanded.height;
 
-	imgExpanded.height = maxHeight; 		// set image height to max
+	imgExpanded.height = maxHeight;         // set image height to max
 	
-	if (imgExpanded.width > maxWidth) { // compare width with max and shrink if necessary
+	if (imgExpanded.width > maxWidth) {     // compare width with max and shrink if necessary
 		imgExpanded.width = maxWidth; 
 		imgExpanded.height = maxWidth/aspectRatio;
 	}
 
 	imgExpanded.style.visibility = 'visible';
 
-    icon.style.left = ($(window).width()-imgExpanded.width)/2+'px';
+	icon.style.left = ($(window).width()-imgExpanded.width)/2+'px';
 	icon.style.visibility = 'visible';
 }
 
 function expand_img(icon) { 
 	imgExpanded = icon.parentElement.previousElementSibling.cloneNode(false);
-    imgExpanded.className = "expanded-img";
-    imgExpanded.id = "expanded-image";
-  	var expandedImgContainer = document.getElementById("expanded-image-container");
-    expandedImgContainer.appendChild(imgExpanded);	
+	imgExpanded.className = "expanded-img";
+	imgExpanded.id = "expanded-image";
+	var expandedImgContainer = document.getElementById("expanded-image-container");
+	expandedImgContainer.appendChild(imgExpanded);	
 	$(imgExpanded).on('load', draw_expanded_img);
 	document.getElementById("expanded-image-outer-veil").style.visibility = 'visible'; 
 }
 
 function close_expanded_img() {
-  	var expandedImgContainer = document.getElementById("expanded-image-container");
-    expandedImgContainer.removeChild(document.getElementById("expanded-image")); 
+	var expandedImgContainer = document.getElementById("expanded-image-container");
+	expandedImgContainer.removeChild(document.getElementById("expanded-image")); 
 	document.getElementById("expanded-image-outer-veil").style.visibility = 'hidden'; 	
 	document.getElementById("expanded-image-close-icon").style.visibility = 'hidden';
-}	
+}
 
 function redraw_img_expanded() {
-	[maxHeight, maxWidth] = [maxWidth, maxHeight]; //swap because of orientation change
-	imgExpanded.height = maxHeight; 			// set image height to max
-	imgExpanded.width = maxHeight*aspectRatio; // width won't set automatically because we undid CSS maxWidth and maxHeight
+	[maxHeight, maxWidth] = [maxWidth, maxHeight];  //swap because of orientation change
+	imgExpanded.height = maxHeight;                // set image height to max
+	imgExpanded.width = maxHeight*aspectRatio;    // width won't set automatically because we undid CSS maxWidth and maxHeight
 
-	if (imgExpanded.width > maxWidth) { // compare width with max and shrink if necessary
+	if (imgExpanded.width > maxWidth) {           // compare width with max and shrink if necessary
 		imgExpanded.width = maxWidth; 
 		imgExpanded.height = maxWidth/aspectRatio;
 	}
@@ -62,7 +62,7 @@ var maxWidth;
 var aspectRatio;
 
 
-window.addEventListener('orientationchange', function() {	
+window.addEventListener('orientationchange', function() {
 	if (imgExpanded) {   redraw_img_expanded(); }
 });
 
--- _includes/post-on-load-script.js
+++ _includes/post-on-load-script.js
@@ -1,23 +1,20 @@
-<script type="text/javascript">
+//<script type="text/javascript">
 
-$(document).ready(function() {
-    //$(".expand-icon").fadeOut(100);
+//$(document).ready(function() {
+	//$(".expand-icon").fadeOut(100);
 	//$(".expand-icon").fadeIn(200);
-    //$(".expand-icon").removeClass('icon-animation').fadeTo(400, 0.3);
-    //$(".expand-icon").addClass('icon-animation')
-    //document.getElementById("expanded-image-outer-veil").style.visibility = 'visible'; 
+	//$(".expand-icon").removeClass('icon-animation').fadeTo(400, 0.3);
+	//$(".expand-icon").addClass('icon-animation')
+	//document.getElementById("expanded-image-outer-veil").style.visibility = 'visible'; 
 
 
 
 	//window.addEventListener("resize", function() {
 	//	document.getElementById("expanded-image-outer-veil").style.visibility = 'visible'; 
 	//}, false);
-      
-      
-      
-    }
-);
+	//}
+//);
 
 
-</script>
+//</script>
 
